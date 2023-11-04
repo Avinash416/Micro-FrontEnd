6fb1683d46d60735430fe79bfe532b1ef12f8b42
AvinashJadhav112 <avinash.j@vervetronics.com>
2023-11-04 12:15:18 +0530

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import  ReactDOM  from 'react-dom'
+import App from './app'
+
+//Mount functon to start up the app
+const mount =(el)=>{
+    ReactDOM.render(
+        <App/>,
+        el
+    )
+
+}
+
+
+//if we are in developement and in isolation,
+//call mount immidiately
+if(process.env.NODE_ENV === 'development'){
+        const devRoot=document.querySelector('#marketing-dev-root')
+        if(devRoot){
+            mount(devRoot)
+        }
+}
+
+//we are running through conatiner
+//amd we should export the mount fnction
+export {mount}
